3ef10661ccafbce4cad14bdc06a647c310878660
jotatimepieces <jotatimepieces@gmail.com>
2026-08-10 14:32:06 -0300

    Atualiza a descrição do produto novamente

--- data/products.js
+++ data/products.js
@@ -35,7 +35,8 @@ export const products = [
       { label: "Fecho", value: "Fecho oculto com botão de pressão" },
       {
         label: "Resistência à Água",
-        value: "Suporta respingos e uso leve no dia a dia. Não indicado para mergulho.",
+        value: "Suporta respingos e uso leve no dia a dia. 
+Não indicado para mergulho.",
       },
       {
         label: "Funções",
